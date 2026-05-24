7e05750ea3edc9e85086e7e85bc1f4749bdb7583
Abim <abimtamang19@gmail.com>
2026-05-24 23:20:21 +0545

    Fix: add custom domain and IP to Vite allowedHosts

--- expense-tracker/vite.config.js
+++ expense-tracker/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    allowedHosts: [
+      'expensetracker.kushal-karki.com.np',
+      '20.40.56.61',
+      'localhost',
+    ],
+  },
 })
